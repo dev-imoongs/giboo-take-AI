8208ec83f832b008d5ec82ed8444a16d6a710d10
dev-imoongs <dev.imoong@gmail.com>
2023-08-10 15:42:58 +0900

    0810 15:42

--- static/js/mypage/write/write_step2.js
+++ static/js/mypage/write/write_step2.js
@@ -1,6 +1,11 @@
 // .lab_choiced가 checkbox on
-
 $('.inp_comm').on('click',(e)=>{
     $('label.lab_comm').removeClass('lab_choiced');
     $(e.target).next().addClass('lab_choiced');
+})
+
+$('.txt_sign').on('click',()=>{
+        $('#modalOFF').attr('id','modalON')
+        $('.dimmed_layer').css('height','100%');
+        $('.dialog-content').css('display','block');
 })
