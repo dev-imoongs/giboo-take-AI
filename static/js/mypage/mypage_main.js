41fbe86395e318df7bebcc4ecb880a3c3c809f1c
dev-ddokddak <139119302+dev-ddokddak@users.noreply.github.com>
2023-08-21 16:20:30 +0900

    main작업중

--- static/js/mypage/mypage_main.js
+++ static/js/mypage/mypage_main.js
@@ -80,3 +80,52 @@ const toastMsg = function (text) {
         }, 1000)
     }
 }
+
+
+// ~분전 관련 스크립트
+document.addEventListener("DOMContentLoaded", function () {
+    // 모든 .txt-time 요소를 찾음
+    let timeElements = document.querySelectorAll(".txt-time");
+
+    // 각 요소에 대해 시간을 계산하여 텍스트로 표시
+    timeElements.forEach(function (element) {
+        let timestamp = element.getAttribute("data-timestamp");
+        let currentTime = Math.floor(Date.now() / 1000); // 현재 시간(초)을 가져옴
+        let timeDifference = currentTime - timestamp;
+
+        let timeText;
+        if (timeDifference < 60) {
+            timeText = timeDifference + "초 전";
+        } else if (timeDifference < 3600) {
+            let minutes = Math.floor(timeDifference / 60);
+            timeText = minutes + "분 전";
+        } else if (timeDifference < 86400) {
+            let hours = Math.floor(timeDifference / 3600);
+            timeText = hours + "시간 전";
+        } else if (timeDifference < 2592000) {  // 30일 이하
+            let days = Math.floor(timeDifference / 86400);
+            if (days == 1) {
+                timeText = "어제";
+            } else {
+                timeText = days + "일 전";
+            }
+        } else if (timeDifference < 7776000) {  // 3달 이하 (90일 * 24시간 * 60분 * 60초)
+            let months = Math.floor(timeDifference / 2592000);
+            if (months == 1) {
+                timeText = "한 달 전";
+            } else if (months == 2) {
+                timeText = "두 달 전";
+            } else if (months == 3) {
+                timeText = "세 달 전";
+            } else {
+                timeText = element.getAttribute("data-timestamp");
+            }
+        } else {
+            let date = new Date(timestamp * 1000);
+            timeText = date.getFullYear() + "." + (date.getMonth() + 1) + "." + date.getDate();
+        }
+
+        // 텍스트를 업데이트
+        element.textContent = timeText;
+    });
+});
