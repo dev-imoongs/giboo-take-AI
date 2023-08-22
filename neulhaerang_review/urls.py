eb8fe69244cfbc834fda902844514a4708456d07
dev-imoongs <dev.imoong@gmail.com>
2023-08-22 23:20:34 +0900

    늘해랑 리뷰 리스트 완료

--- neulhaerang_review/urls.py
+++ neulhaerang_review/urls.py
@@ -1,10 +1,20 @@
 from django.urls import path
 
-from neulhaerang_review.views import NeulhaerangReviewListView, NeulhaerangReviewDetailView
+from neulhaerang_review.views import NeulhaerangReviewListView, NeulhaerangReviewDetailView, \
+    NeulhaerangReviewListAPIView
 
 app_name = 'neulhaerang_review'
 
 urlpatterns = [
+
+
     path('review/list/', NeulhaerangReviewListView.as_view(),name='review/list'),
-    path('review/detail/', NeulhaerangReviewDetailView.as_view(),name='review/detail'),
+    path('review/detail/<int:neulhaerang_review_id>', NeulhaerangReviewDetailView.as_view(),name='review/detail'),
+
+
+
+#     api view
+    path('review-list-api-view/', NeulhaerangReviewListAPIView.as_view(), name='review-list-api-view'),
+
+
 ]
--- static/js/neulhaerang/review-list.js
+++ static/js/neulhaerang/review-list.js
@@ -23,3 +23,76 @@ $(".list-cate li").each((i,cate)=>{
       })
     })
   })
+
+let page = 1
+let sort = "추천순"
+
+// 클릭한 sort값 sort에 입력(추천순, 최신순, 종료임박순)
+$('.lab_sort').each((i,v)=>{
+    $(v).on('click',()=>{
+        sort = $(v).text()
+        console.log(sort)
+        page=1
+        showNeulhaerangReviewList(page, sort)
+    })
+})
+
+const showNeulhaerangReviewList = (page, sort, scroll)=>{
+  fetch(`/neulhaerang_review/review-list-api-view/?page=${page}&sort=${sort}`)
+      .then(response=> response.json())
+      .then(result=>{
+        let text = ""
+        let posts= result.posts
+        let pagenator= result.pagenator
+
+        posts.forEach((post,i)=> {
+
+              let percentage = Math.ceil(post.donation_amount_sum / post.target_amount *100)
+              const post_url2 = baseUrl2.replace(0, neulhaerang_review_id=post.id);
+              text += `<li class="listcard">
+                    <a href="${post_url2}" class="link_pack">
+                    <span class="box_thumb">
+                      <span kagetype="c203" class="img_thumb" style="background-image: url(${post.thumbnail_image});"></span>
+                      </span>
+                      <span class="box_together">
+                      <span class="bundle_tit">
+                        <strong class="tit_together ellipsis_type1">
+                            ${post.review_title}
+                        </strong>
+                        <span class="txt_proposer"> ${post.member_nickname} </span>
+
+                      </span>
+                      <span class="wrap_state">
+                        <span class="state_bar">
+                          <span class="state_gage state_ing" style="width: ${percentage}%"></span>
+                        </span>
+                      </span>`;
+                      if(post.donation_amount_sum !== null){
+                        text += `<span class="price_goal">${post.donation_amount_sum.toLocaleString("ko-KR")}원</span>
+                    </span>
+                  </a>
+              </li>`
+                      } else{
+                        text += `<span class="price_goal">0원</span>
+                    </span>
+                  </a>
+              </li>`
+                      }
+          })
+            scroll? $('.list_fund').append(text): $('.list_fund').html(text)
+
+
+      })
+
+}
+showNeulhaerangReviewList(page, sort)
+window.addEventListener("scroll", ()=>{
+    console.log(window.innerHeight)
+    console.log(window.scrollY)
+    console.log(document.body.clientHeight)
+
+    if (window.innerHeight + window.scrollY + 1 >= document.body.clientHeight) {
+        page++
+        showNeulhaerangReviewList(page, sort,"scroll")
+}
+});
