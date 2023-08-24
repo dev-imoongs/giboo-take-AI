22dfe1c648a4bbc091ff6cee82113be4aff31b44
SilverSharkDeveloper <Dev.silver.shark@gmail.com>
2023-08-24 14:47:22 +0900

    asdf

--- introduce/urls.py
+++ introduce/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 
+from introduce.views import IntroduceAboutView, IntroduceGibooSuggestionView, IntroducePartnersView, \
+    IntroduceStatisticsView
+
 app_name = 'introduce'
 
 urlpatterns = [
-
+    path('about/' ,IntroduceAboutView.as_view(),name="about" ),
+    path('giboo-suggestion/',IntroduceGibooSuggestionView.as_view(),name="giboo-suggestion"),
+    path('partners/',IntroducePartnersView.as_view(),name="partners"),
+    path('statistics/',IntroduceStatisticsView.as_view(),name="statistics"),
 ]
--- introduce/views.py
+++ introduce/views.py
@@ -1,6 +1,43 @@
+from django.db.models import Sum
 from django.shortcuts import render
+from django.views import View
 
+from neulhaerang.models import Neulhaerang, NeulhaerangLike, NeulhaerangDonation
+from neulhajang.models import Neulhajang, NeulhajangLike
 from workspace.models import Period
 
 
 # Create your views here.
+class IntroduceAboutView(View):
+    def get(self,request):
+        neulhaerang_count  = Neulhaerang.objects.all().count()
+        neulhajang_count  = Neulhajang.objects.all().count()
+        neulhaerang_like_count  = NeulhaerangLike.objects.all().count()
+        neulhajang_like_count  = NeulhajangLike.objects.all().count()
+        donation_total = NeulhaerangDonation.objects.aggregate(total = Sum("donation_amount"))["total"]
+        project_total = neulhajang_count+neulhaerang_count
+        like_total = neulhajang_like_count+neulhaerang_like_count
+        average = donation_total//project_total
+
+        datas = {
+            "project_total":project_total,
+            "donation_total":donation_total,
+            "like_total":like_total,
+            "average":average,
+
+        }
+        return render(request,"introduce/about.html",datas)
+
+
+class IntroduceGibooSuggestionView(View):
+    def get(self,request):
+        return render(request,"introduce/giboo-suggestion.html")
+
+
+class IntroducePartnersView(View):
+    def get(self,request):
+        return render(request,"introduce/partners.html")
+
+class IntroduceStatisticsView(View):
+    def get(self,request):
+        return render(request,"introduce/statistics.html")
