bce850a359ff13e9fa62abd33d33adbd00ec5f60
bbori.Kim <dev.bbori@gmail.com>
2023-08-24 11:48:46 +0900

    로그인완료

--- member/urls.py
+++ member/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
-from django.views.generic import TemplateView
-
-from main.views import MainView
-from member.views import LoginView, LogoutView
+from member.views import LoginView, LogoutView, LoginAPIView
 
 app_name = 'member'
 
 urlpatterns = [
-    path('login/', LoginView.as_view(), name='redirect'),
+    path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
+
+    # api view
+    path('reset/', LoginAPIView.as_view(), name='reset')
 ]
