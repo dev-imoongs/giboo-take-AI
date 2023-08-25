db4dfaaefe97238901f5b1ef310e26d3d158f84a
bbori.Kim <dev.bbori@gmail.com>
2023-08-25 16:09:01 +0900

    search form tag 완료

--- search/urls.py
+++ search/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path
 
-from search.views import SearchFormView, SearchClickView, SearchInputView, ShowTagAPIView
+from search.views import SearchFormView, SearchClickView, SearchInputView, ShowTagAPIView, ShowCategoryAPIView
 
 app_name = 'search'
 
 urlpatterns = [
     path('form/',SearchFormView.as_view(),name='form'),
-    path('click/<str:category_name>',SearchClickView.as_view(),name='click'),
+    path('click/',SearchClickView.as_view(),name='click'),
     path('input/',SearchInputView.as_view(),name='input'),
+
     # API View
-    path('tag/', ShowTagAPIView.as_view(), name='tag')
+    path('tag/', ShowTagAPIView.as_view(), name='tag'),
+    path('category/', ShowCategoryAPIView.as_view(), name='category')
 ]
--- static/js/search/search-form.js
+++ static/js/search/search-form.js
@@ -15,6 +15,50 @@ $searchInput.on("input",e=>{
     }
 })
 
-const showTag = (tag) => {
+
+$(document).ready(()=> {
+    showTag();
+    showCategory();
+})
+
+
+const showTag = () => {
     fetch(`/search/tag/`)
+        .then(response => response.json())
+        .then(result => {
+            let tags = result.tags;
+            let text = "";
+
+            tags.forEach((tag, i) => {
+                let name = tag.tag_name;  // 'tag_name'을 직접 가져옴
+                let type = tag.tag_type;  // 'tag_type'을 직접 가져옴
+
+                text += `<tag-card><a class="link_hash hash_type${type}">#${name}</a></tag-card>`;
+            })
+            $('.hash_group').html(text);
+        });
+};
+
+
+const showCategory = () => {
+    fetch(`/search/category/`)
+        .then(response => response.json())
+        .then(result => {
+            let categories = result.categories
+            let text = "";
+
+            categories.forEach((category, i) => {
+                let name = category.category_name;
+
+                text += `
+                    <li>
+                      <a class="link_category">
+                        <img class="img_thumb" src="https://mud-kage.kakaocdn.net/dn/eE4eDt/btqeTEvo1oi/i3hCyjx9BasmNFN6Qnewz0/c160.jpg"/>
+                        ${name}
+                      </a>
+                    </li>`
+            })
+            $(".list_category").html(text)
+        })
+
 }
